a852c19b5622f6805b78bda28c251ee72743f35c
Elizabeth <hyliu12@myseneca.ca>
2022-10-08 21:48:06 -0400

    Added do-while loop for going back to menu options.

--- main.cpp
+++ main.cpp
@@ -15,29 +15,37 @@ int main() {
     int option;
     double balance = 500;
 
-    showMenu();
-    cout << "o p t i o n :  ";
-    cin >> option;  // prompt for option selection
+    do {
 
-    switch(option) {
-        case 1: cout << "Balance is:  " << balance << " $" << endl;
-            break;
-        case 2: cout << "Deposit Amount:  ";
-            double depositAmount;
-            cin >> depositAmount;
-            balance += depositAmount;
-            break;
-        case 3: cout << "Withdraw Amount:  ";
-            double withdrawAmount;
-            cin >> withdrawAmount;
-            if (withdrawAmount <= balance) {
-                balance -= withdrawAmount;
-            } else {
-                cout << "Insufficient Funds!" << endl;
-            }
-            break;
-    }
+        showMenu();
+        cout << "o p t i o n :  ";
+        cin >> option;  // prompt for option selection
 
-    //system("pause>0");
+        //system("cls");  // used to make screen/terminal clear
+
+        switch (option) {
+            case 1:
+                cout << "Balance is:  " << balance << " $" << endl;
+                break;
+            case 2:
+                cout << "Deposit Amount:  ";
+                double depositAmount;
+                cin >> depositAmount;
+                balance += depositAmount;
+                break;
+            case 3:
+                cout << "Withdraw Amount:  ";
+                double withdrawAmount;
+                cin >> withdrawAmount;
+                if (withdrawAmount <= balance) {
+                    balance -= withdrawAmount;
+                } else {
+                    cout << "Insufficient Funds!" << endl;
+                }
+                break;
+        }
+    } while (option != 4);
+
+    //system("pause>0");  // for execute pause command and make screen/terminal wait for key press
 
 }
